e78380888ae180eecfb9be34aa4e7195e2b85a18
rhantj <dddddfgh@gmail.com>
2026-02-02 12:32:45 +0900

    feat : facility 중간 저장

--- Assets/01_Scripts/Facility/ProductionFacility.cs
+++ Assets/01_Scripts/Facility/ProductionFacility.cs
@@ -4,16 +4,50 @@ using UnityEngine;
 public class ProdictionFacility : MonoBehaviour
 {
     [SerializeField] private SOProductionFacility m_FacilitySO;
+    [SerializeField] private ItemIOArea m_OutputArea;
     [SerializeField] private ItemIOArea m_InputArea;
 
     private int m_Input;
     private int m_Output;
+    private Coroutine m_ProductionCoroutine;
 
     public SOProductionFacility FacilitySO { get { return m_FacilitySO; } }
 
     private void Awake()
     {
         InitializeIOProduct(m_FacilitySO);
+    }
+
+    private void OnEnable()
+    {
+        if (m_OutputArea)
+        {
+            m_OutputArea.m_OnEnterArea += PlayerEnter;
+            m_OutputArea.m_OnExitArea += PlayerExit;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (m_OutputArea)
+        {
+            m_OutputArea.m_OnEnterArea -= PlayerEnter;
+            m_OutputArea.m_OnExitArea -= PlayerExit;
+        }
+    }
+
+    private void PlayerEnter()
+    {
+        m_ProductionCoroutine = StartCoroutine(WaitForOutput());
+    }
+
+    private void PlayerExit()
+    {
+        if (m_ProductionCoroutine != null)
+        {
+            StopCoroutine(m_ProductionCoroutine);
+            m_ProductionCoroutine = null;
+        }
     }
 
     private void InitializeIOProduct(SOProductionFacility data)
@@ -24,6 +58,7 @@ public class ProdictionFacility : MonoBehaviour
 
     private IEnumerator WaitForOutput()
     {
+        Debug.Log("item product routine start");
         float elapsedTime = 0f;
         while (elapsedTime < .5f)
         {
@@ -31,7 +66,9 @@ public class ProdictionFacility : MonoBehaviour
             yield return null;
         }
 
-        // return output
+        GiveItemToPlayer();
+
+        m_ProductionCoroutine = null;
     }
 
     private IEnumerator WaitForInput()
@@ -44,5 +81,10 @@ public class ProdictionFacility : MonoBehaviour
         }
 
         // return input
+    }
+
+    private void GiveItemToPlayer()
+    {
+        Debug.Log($"Return : {m_Output}");
     }
 }
