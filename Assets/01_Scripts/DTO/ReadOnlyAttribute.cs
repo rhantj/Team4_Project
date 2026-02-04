974eb2a6dfbc253250bb88c95a057cc0853e803a
Seungyoon Lee <webmaster@cygnus.stream>
2026-02-04 10:32:40 +0900

    chore: ReadOnlyAttribute에 간단한 설명 주석 추가

--- Assets/01_Scripts/DTO/ReadOnlyAttribute.cs
+++ Assets/01_Scripts/DTO/ReadOnlyAttribute.cs
@@ -1,6 +1,9 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+/// Readonly attribute to prevent changes in the Unity inspector.
+/// </summary>
 [AttributeUsage(AttributeTargets.Field)]
 public class ReadOnlyAttribute : PropertyAttribute
 {
