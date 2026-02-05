2006424f65a7b220ead9c17b483f9a3e88267956
JJ\wls15 <wls1584@naver.com>
2026-02-05 15:45:50 +0900

    feat/Inventory : SO데이터를 받아서 캐릭터가 SO에 맞는 프리팹을 생성 기초자원에셋모양 추가 및 수정

--- Assets/01_Scripts/JJH/Inventory/Inventory.cs
+++ Assets/01_Scripts/JJH/Inventory/Inventory.cs
@@ -1,113 +1,55 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Inventory : MonoBehaviour
 {
     //용량
     [SerializeField] private int capacity = 10;
-    //짊어질 아이템
-    [SerializeField] private GameObject itemPrefab;
     //아이템 위치
     [SerializeField] private Transform itemSpawnPoint;
-    //아이템 간격
-    [SerializeField] private float stackInterval = 1f;
+
+    public bool IsFull => stackItem.Count >= capacity;
 
     private List<GameObject>stackItem=new List<GameObject>();
 
-
-
-    private float movespeed = 5f;
-    
-    public void AddItem()
+    public void AddItem(ResourceItemData data)
     {
-        if(stackItem.Count>=capacity)
+        //가득참 체크
+        if (IsFull)
         {
-            Debug.Log("인벤토리 꽉참");
+            Debug.Log("인벤토리 가득참");
             return;
         }
+        //아이템 생성
+        GameObject resourceItem = Instantiate(data.itemPrefab);
+        resourceItem.transform.SetParent(itemSpawnPoint,false);
 
-        //아이템생성위치관련
-        Vector3 spawnPos = itemSpawnPoint.position + (itemSpawnPoint.up * stackInterval * stackItem.Count);
-        GameObject newItem = Instantiate(itemPrefab, spawnPos, Quaternion.identity);
-        
-        
-        newItem.transform.SetParent(itemSpawnPoint);
+        //아이템 높이계산
+        float height = 0f;
+        foreach(GameObject obj in stackItem)
+        {
+            height+=obj.GetComponentInChildren<Renderer>().bounds.size.y;
+        }
 
-        stackItem.Add(newItem);
+        resourceItem.transform.localPosition = new Vector3(0, height, 0);
+
+
+        stackItem.Add(resourceItem);
     }
     public void RemoveItem()
     {
-        if(stackItem.Count==0)
+        if (stackItem.Count > 0)
         {
-            Debug.Log("인벤토리 비어있음");
-            return;
+            int lastIndex = stackItem.Count - 1;
+            GameObject removeItem=stackItem[lastIndex];
+
+            stackItem.RemoveAt(lastIndex);
+
+            //제거는 나중에 수정할예정
+            Destroy(removeItem);
+
+
         }
-        GameObject removedItem = stackItem[stackItem.Count - 1];
-        stackItem.RemoveAt(stackItem.Count - 1);
-        Destroy(removedItem);
     }
-    public void AddItem2()
-    {
-        if(stackItem.Count >= capacity)
-        {
-            Debug.Log("인벤토리 꽉참");
-            return;
-        }
-
-        GameObject newItem = Instantiate(itemPrefab);
-        newItem.transform.SetParent(itemSpawnPoint, false);
-
-        float totalOffset = 0f;
-        for(int i = 0; i < stackItem.Count; i++)
-        {
-            totalOffset+= ItemHeight(stackItem[i]);
-        }
-
-        newItem.transform.localPosition = new Vector3(0, totalOffset, 0);
-        stackItem.Add(newItem);
-    }
-    private float ItemHeight(GameObject obj)
-    {
-        Renderer renderer = obj.GetComponent<Renderer>();
-        if(renderer != null)
-        {
-            return renderer.bounds.size.y;
-        }
-        return stackInterval;
-
-    }
-
-
-
-    //테스트용 쌓기
-    private void Update()
-    {
-        
-        if(Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-            AddItem2();
-            Debug.Log(stackItem.Count);
-            Debug.Log(stackItem.Capacity);
-            Debug.Log("스페이스누름");
-        }
-
-        //move();
-
-    }
-
-    private void move()
-    {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        Vector3 dir = new Vector3(h, 0, v).normalized;
-        transform.Translate(dir * movespeed * Time.deltaTime);
-    }
-    
-
-
-
-
 
 }
--- Assets/01_Scripts/JJH/Inventory/SupplyStation.cs
+++ Assets/01_Scripts/JJH/Inventory/SupplyStation.cs
@@ -2,36 +2,36 @@ using UnityEngine;
 
 public class SupplyStation : MonoBehaviour
 {
-
+    //리소스 아이템SO
+    [SerializeField]private ResourceItemData SOItemData;
+    //공급 간격(낮을수록빨라짐)
     [SerializeField]private float supplyInterval = 1f;
     private float supplyTimer = 0f;
-
     
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            supplyTimer+=Time.deltaTime;
-            if(supplyTimer>=supplyInterval)
-            {
-                Inventory playerInventory = other.GetComponent<Inventory>();
+            Inventory inven=other.GetComponent<Inventory>();
 
-                if(playerInventory != null)
+            if (inven != null&&!inven.IsFull)
+            {
+                supplyTimer += Time.deltaTime;
+                if(supplyTimer >= supplyInterval)
                 {
-                    playerInventory.AddItem2();
-                    
+                    inven.AddItem(SOItemData);
+                    supplyTimer = 0f;
                 }
-                supplyTimer=0f;
             }
-            
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            supplyTimer=0f;
+            supplyTimer = 0f;
         }
     }
+        
 }
