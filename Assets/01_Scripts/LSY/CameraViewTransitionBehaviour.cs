ee89509e76b8640180e99bfb5cb3d70188183607
Seungyoon Lee <webmaster@cygnus.stream>
2026-02-24 14:54:10 +0900

    feat: 설명을 위한 기즈모 추가

--- Assets/01_Scripts/LSY/CameraViewTransitionBehaviour.cs
+++ Assets/01_Scripts/LSY/CameraViewTransitionBehaviour.cs
@@ -37,6 +37,15 @@ public class CameraViewTransitionBehaviour : MonoBehaviour
 
     private Camera m_thisCamera;
 
+    private Vector3 m_DebugLookAtOnXZ;
+    private Vector3 m_DebugLookAtOffset;
+    private Vector3 m_DebugLookAt;
+    private Vector3 m_DebugCameraOffset;
+    private Vector3 m_DebugTargetCameraPosition;
+    private Vector3 m_DebugResultCameraPosition;
+    Mesh m_DebugLookOffsetGizmoMesh;
+    Mesh m_DebugCameraOffsetGizmoMesh;
+
     private void Awake()
     {
         m_thisCamera = GetComponent<Camera>();
@@ -77,43 +86,121 @@ public class CameraViewTransitionBehaviour : MonoBehaviour
         if (null == PlayerCharacterTransform) return;
         if (null == MainBuildingTransform) return;
 
+        // 주 건물과 플레이어 사이 distance가 커브 적용 거리 구간 위에서 어느 지점에 존재하는지 계산해 커브값 적용
         Vector2 mainBuildingPositionOnXZ = new Vector2(MainBuildingTransform.position.x, MainBuildingTransform.position.z);
         Vector2 playerCharacterPositionOnXZ = new Vector2(PlayerCharacterTransform.position.x, PlayerCharacterTransform.position.z);
         float planarDistanceOnXZ = Vector2.Distance(mainBuildingPositionOnXZ, playerCharacterPositionOnXZ);
-
         RawRatio = Mathf.InverseLerp(MinimumDistance, MaximumDistance, planarDistanceOnXZ);
-        MixRatio = Mathf.Clamp(m_MixingCurve.Evaluate(RawRatio), Mathf.Min(m_ClampValue1, m_ClampValue2), Mathf.Max(m_ClampValue1, m_ClampValue2));
+        MixRatio = Mathf.Clamp(m_MixingCurve.Evaluate(RawRatio),
+                               Mathf.Min(m_ClampValue1, m_ClampValue2),
+                               Mathf.Max(m_ClampValue1, m_ClampValue2));
 
+        // 두 목표 사이 비율에 따라 보간된 지점으로부터 보간된 LookAt 오프셋 적용해 바라보는 지점 계산
         Vector2 lookAtOnXZ = Vector2.Lerp(mainBuildingPositionOnXZ, playerCharacterPositionOnXZ, MixRatio);
-
         Vector3 lookAtOffset = Vector3.Lerp(m_EndLookAtOffset, m_StartLookAtOffset, MixRatio);
         Vector3 lookAt = new Vector3(lookAtOnXZ.x, 0, lookAtOnXZ.y) + lookAtOffset;
+
+        // 바라보는 지점으로부터 보간된 카메라 오프셋 적용하여 카메라 위치 계산
         Vector3 cameraOffset = Vector3.Lerp(m_EndCameraOffset, m_StartCameraOffset, MixRatio);
-        Vector3 resultPosition = lookAt + cameraOffset;
-        Quaternion resultRotation = Quaternion.LookRotation(-cameraOffset);
+        Vector3 targetCameraPosition = lookAt + cameraOffset;
+        Quaternion targetCameraRotation = Quaternion.LookRotation(-cameraOffset);
 
-        float defaultTangent = Mathf.Tan(m_StartFieldOfView * Mathf.Deg2Rad * 0.5f);
-        float transitionedTangent = Mathf.Tan(m_EndFieldOfView * Mathf.Deg2Rad * 0.5f);
-        float tangent = Mathf.Lerp(transitionedTangent, defaultTangent, MixRatio);
+        // 카메라 화각은 tangent를 보간하여 역으로 얻어냄
+        float defaultFoVTangent = Mathf.Tan(m_StartFieldOfView * Mathf.Deg2Rad * 0.5f);
+        float transitionedFoVTangent = Mathf.Tan(m_EndFieldOfView * Mathf.Deg2Rad * 0.5f);
+        float targetFoVTangent = Mathf.Lerp(transitionedFoVTangent, defaultFoVTangent, MixRatio);
 
+        // 카메라 움직임 보간 수치 계산
         float interpolationRate = Mathf.Exp(-(Time.deltaTime / SmoothTime));
-
 #if UNITY_EDITOR
         if (!Application.isPlaying) interpolationRate = 0f;
 #endif
 
-        resultPosition = Vector3.Lerp(resultPosition, m_thisCamera.transform.position, interpolationRate);
-        resultRotation = Quaternion.Slerp(resultRotation, m_thisCamera.transform.rotation, interpolationRate);
-        float currentTangent = Mathf.Tan(m_thisCamera.fieldOfView * Mathf.Deg2Rad * 0.5f);
-        float resultTangent = Mathf.Lerp(tangent, currentTangent, interpolationRate);
-        float fieldOfView = Mathf.Atan(resultTangent) * Mathf.Rad2Deg * 2f;
+        // 카메라가 부드럽게 움직이도록 보간
+        Vector3 resultCameraPosition = Vector3.Lerp(targetCameraPosition, m_thisCamera.transform.position, interpolationRate);
+        Quaternion resultCameraRotation = Quaternion.Slerp(targetCameraRotation, m_thisCamera.transform.rotation, interpolationRate);
+        float currentFoVTangent = Mathf.Tan(m_thisCamera.fieldOfView * Mathf.Deg2Rad * 0.5f);
+        float resultFoVTangent = Mathf.Lerp(targetFoVTangent, currentFoVTangent, interpolationRate);
+        float fieldOfView = Mathf.Atan(resultFoVTangent) * Mathf.Rad2Deg * 2f;
 
 #if UNITY_EDITOR
+        m_DebugLookAtOnXZ = new Vector3(lookAtOnXZ.x, 0, lookAtOnXZ.y);
+        m_DebugLookAtOffset = lookAtOffset;
+        m_DebugLookAt = lookAt;
+        m_DebugCameraOffset = cameraOffset;
+        m_DebugTargetCameraPosition = targetCameraPosition;
+        m_DebugResultCameraPosition = resultCameraPosition;
+
         if (float.IsNaN(fieldOfView)) Debug.LogError("Calculated Field of View was NaN.");
 #endif
         if (float.IsNaN(fieldOfView)) fieldOfView = 60f;
 
-        m_thisCamera.transform.SetPositionAndRotation(resultPosition, resultRotation);
+        m_thisCamera.transform.SetPositionAndRotation(resultCameraPosition, resultCameraRotation);
         m_thisCamera.fieldOfView = fieldOfView;
     }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        m_DebugLookOffsetGizmoMesh = new Mesh();
+        m_DebugLookOffsetGizmoMesh.vertices = new Vector3[3]
+        {
+            Vector3.zero,
+            m_StartLookAtOffset,
+            m_EndLookAtOffset
+        };
+        m_DebugLookOffsetGizmoMesh.triangles = new int[6] { 0, 1, 2, 2, 1, 0 };
+        m_DebugLookOffsetGizmoMesh.normals = new Vector3[3] { Vector3.zero, Vector3.zero, Vector3.zero };
+
+        m_DebugCameraOffsetGizmoMesh = new Mesh();
+        m_DebugCameraOffsetGizmoMesh.vertices = new Vector3[3]
+        {
+            Vector3.zero,
+            m_StartCameraOffset,
+            m_EndCameraOffset
+        };
+        m_DebugCameraOffsetGizmoMesh.triangles = new int[6] { 0, 1, 2, 2, 1, 0 };
+        m_DebugCameraOffsetGizmoMesh.normals = new Vector3[3] { Vector3.zero, Vector3.zero, Vector3.zero };
+    }
+
+    private void OnDrawGizmos()
+    {
+        MoveCamera();
+
+        Color prevColor = Gizmos.color;
+
+
+        Vector3 mainBuildingPositionOnXZ = new Vector3(MainBuildingTransform.position.x, 0f, MainBuildingTransform.position.z);
+        Vector3 playerCharacterPositionOnXZ = new Vector3(PlayerCharacterTransform.position.x, 0f, PlayerCharacterTransform.position.z);
+
+        Gizmos.color = Color.magenta;   Gizmos.DrawLine(m_DebugLookAtOnXZ, mainBuildingPositionOnXZ);
+        Gizmos.color = Color.cyan;      Gizmos.DrawLine(m_DebugLookAtOnXZ, playerCharacterPositionOnXZ);
+
+        Gizmos.color = new Color(Color.pink.r, Color.pink.g, Color.pink.b, 0.5f);
+        Gizmos.DrawMesh(m_DebugLookOffsetGizmoMesh, m_DebugLookAtOnXZ);
+
+        Gizmos.color = new Color(Color.skyBlue.r, Color.skyBlue.g, Color.skyBlue.b, 0.5f);
+        Gizmos.DrawMesh(m_DebugCameraOffsetGizmoMesh, m_DebugLookAt);
+
+        Gizmos.color = Color.pink;
+        Gizmos.DrawRay(m_DebugLookAtOnXZ, m_StartLookAtOffset);
+        Gizmos.DrawRay(m_DebugLookAtOnXZ, m_EndLookAtOffset);
+
+        Gizmos.color = Color.skyBlue;
+        Gizmos.DrawRay(m_DebugLookAt, m_StartCameraOffset);
+        Gizmos.DrawRay(m_DebugLookAt, m_EndCameraOffset);
+
+        Gizmos.color = Color.white;     Gizmos.DrawRay(m_DebugTargetCameraPosition, -100f * m_DebugCameraOffset);
+        Gizmos.color = Color.red;       Gizmos.DrawLine(m_DebugLookAtOnXZ, m_DebugLookAt);
+        Gizmos.color = Color.blue;      Gizmos.DrawLine(m_DebugTargetCameraPosition, m_DebugLookAt);
+        Gizmos.color = Color.green;     if (Application.isPlaying) Gizmos.DrawLine(m_DebugResultCameraPosition, m_DebugTargetCameraPosition);
+
+        Gizmos.color = Color.white;     Gizmos.DrawSphere(m_DebugLookAtOnXZ, 0.5f);
+        Gizmos.color = Color.red;       Gizmos.DrawSphere(m_DebugLookAt, 0.5f);
+        Gizmos.color = Color.blue;      Gizmos.DrawSphere(m_DebugTargetCameraPosition, 0.5f);
+        Gizmos.color = Color.green;     if (Application.isPlaying) Gizmos.DrawSphere(m_DebugResultCameraPosition, 0.5f);
+
+        Gizmos.color = prevColor;
+    }
+#endif
 }
