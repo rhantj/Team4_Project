f5e9943f12cf417de345b4a5038b9af9ac7c8614
rhantj <dddddfgh@gmail.com>
2026-03-15 22:16:06 +0900

    feat : 5스테이지까지 맵 추가. 메인 로비로 돌아갈 때 생기는 IO Area 버그 픽스

--- Assets/01_Scripts/Area/PlayerAreaDetector.cs
+++ Assets/01_Scripts/Area/PlayerAreaDetector.cs
@@ -7,6 +7,7 @@ public class PlayerAreaDetector : MonoBehaviour
     [SerializeField] private float m_CheckInterval = .1f;
 
     private Coroutine m_CheckCo;
+    private List<ItemIOArea> m_Areas = new();
 
     private void Start()
     {
@@ -20,10 +21,11 @@ public class PlayerAreaDetector : MonoBehaviour
         while (true)
         {
             Vector3 playerPos = transform.position;
-            List<ItemIOArea> areas = SpatialHashManager.Instance.Query(playerPos);
+            m_Areas = SpatialHashManager.Instance.Query(playerPos);
 
-            foreach(var area in areas)
+            foreach(var area in m_Areas)
             {
+                if (!area) continue;
                 bool isInside = area.CheckPlayer(playerPos);
 
                 if (isInside && !area.IsPlayerEnter)
